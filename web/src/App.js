3fb169ba9ee7f8bd0394111cb4f2c0d8bd099a6a
edvinpontuslarsson <el223na@student.lnu.se>
2021-04-07 21:04:51 +0200

    simple display of course names

--- web/src/App.js
+++ web/src/App.js
@@ -1,29 +1,22 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 import axios from 'axios';
 
 function App() {
-  axios.get('/api').then((payload) => {
-    console.log(payload.data);
-  });
+  const [courses, setCourses] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api').then((payload) => {
+      setCourses(payload.data);
+    });
+  }, [courses]);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {courses.map((item) => (
+        <p>{item.name}</p>
+      ))}
+    </>
   );
 }
 
