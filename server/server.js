2197c24ad399084ccba0b88f1e4b491973dfd39d
edvinpontuslarsson <el223na@student.lnu.se>
2021-04-08 20:07:41 +0200

    removes mongoose message test

--- server/server.js
+++ server/server.js
@@ -2,8 +2,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const fs = require('fs');
 
-const Message = require('./models/Message');
-
 const app = express();
 
 app.use(express.json());
