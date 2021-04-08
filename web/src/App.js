3d3d2683d04eed55284614f4e7c2325750afec83
Edvin Larsson <el223na@student.lnu.se>
2021-04-08 15:34:21 +0200

    Post selected course and date (#10)
    * fixes useEffect dependency bug
    
    * no more display all course names
    
    * select course name
    
    * fixes selected course
    
    * renames courses var
    
    * comment color codes
    
    * select course date options
    
    * function for filtering duplicate dates
    
    * removes a console log
    
    * dates show up select but not changeable
    
    * course dates select works

--- web/src/App.js
+++ web/src/App.js
@@ -4,45 +4,90 @@ import axios from 'axios';
 
 function App() {
   const [courses, setCourses] = useState([]);
+
+  const [selectedCourse, setSelectedCourse] = useState({});
+  const [selectedDate, setSelectedDate] = useState('');
+
   const [companyName, setCompanyName] = useState('');
   const [companyPhone, setCompanyPhone] = useState('');
   const [companyEmail, setCompanyEmail] = useState('');
 
   useEffect(() => {
     axios.get('/api').then((payload) => {
-      setCourses(payload.data);
+      console.log(payload.data);
+
+      const coursesData = payload.data;
+
+      setCourses(coursesData);
+
+      // initial default course selection
+      setSelectedCourse(coursesData[0]);
+      setSelectedDate(coursesData[0]?.dates[0]);
     });
-  }, [courses]);
+  }, []);
+
+  const getCourse = (courseName) =>
+    courses.filter(
+      (course) => course.name === courseName
+    )[0];
 
   return (
     <>
-      {courses.map((item) => (
-        <p>{item.name}</p>
-      ))}
-      <input
-        type="text"
-        value={companyName}
-        placeholder="Name"
-        onChange={(input) =>
-          setCompanyName(input.target.value)
-        }
-      />
-      <input
-        type="text"
-        value={companyPhone}
-        placeholder="Phone"
-        onChange={(input) =>
-          setCompanyPhone(input.target.value)
-        }
-      />
-      <input
-        type="text"
-        value={companyEmail}
-        placeholder="Email"
-        onChange={(input) =>
-          setCompanyEmail(input.target.value)
-        }
-      />
+      <div>
+        <select
+          value={selectedCourse.name}
+          onChange={(input) => {
+            const courseName = input.target.value;
+            const course = getCourse(courseName);
+
+            setSelectedCourse(course);
+          }}
+        >
+          {courses?.map((course) => (
+            <option key={course.id} value={course.name}>
+              {course.name}
+            </option>
+          ))}
+        </select>
+        <select
+          value={selectedDate}
+          onChange={(input) =>
+            setSelectedDate(input.target.value)
+          }
+        >
+          {selectedCourse?.dates?.map((date) => (
+            <option key={date} value={date}>
+              {date}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div>
+        <input
+          type="text"
+          value={companyName}
+          placeholder="Name"
+          onChange={(input) =>
+            setCompanyName(input.target.value)
+          }
+        />
+        <input
+          type="text"
+          value={companyPhone}
+          placeholder="Phone"
+          onChange={(input) =>
+            setCompanyPhone(input.target.value)
+          }
+        />
+        <input
+          type="text"
+          value={companyEmail}
+          placeholder="Email"
+          onChange={(input) =>
+            setCompanyEmail(input.target.value)
+          }
+        />
+      </div>
       <button
         onClick={() => {
           axios.post('/api', {
@@ -51,6 +96,8 @@ function App() {
                 companyName,
                 companyPhone,
                 companyEmail,
+                courseName: selectedCourse.name,
+                selectedDate,
               },
             },
           });
