1258bc2b6807bda77869f4822793afd2b4cde891
edvinpontuslarsson <el223na@student.lnu.se>
2021-04-09 20:22:25 +0200

    label-and-input columns

--- web/src/App.js
+++ web/src/App.js
@@ -3,6 +3,10 @@ import './App.css';
 import axios from 'axios';
 import { v4 as uuid } from 'uuid';
 
+// classnames
+const courseAndDate = 'course-and-date';
+const labelAndInput = 'label-and-input';
+
 function App() {
   const [courses, setCourses] = useState([]);
 
@@ -86,82 +90,95 @@ function App() {
   return (
     <main>
       <h2>Course</h2>
-      <label>NAME</label>
-      <select
-        value={selectedCourse.name}
-        onChange={(event) => {
-          const courseName = event.target.value;
-          const course = getCourse(courseName);
+      <div className={courseAndDate}>
+        <div className={labelAndInput}>
+          <label>NAME</label>
+          <select
+            value={selectedCourse.name}
+            onChange={(event) => {
+              const courseName = event.target.value;
+              const course = getCourse(courseName);
 
-          setSelectedCourse(course);
-        }}
-      >
-        {courses?.map((course) => (
-          <option key={course.id} value={course.name}>
-            {course.name}
-          </option>
-        ))}
-      </select>
-      <label>DATE</label>
-      <select
-        value={selectedDate}
-        onChange={(event) =>
-          setSelectedDate(event.target.value)
-        }
-      >
-        {selectedCourse?.dates?.map((date) => (
-          <option key={date} value={date}>
-            {date}
-          </option>
-        ))}
-      </select>
+              setSelectedCourse(course);
+            }}
+          >
+            {courses?.map((course) => (
+              <option key={course.id} value={course.name}>
+                {course.name}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className={labelAndInput}>
+          <label>DATE</label>
+          <select
+            value={selectedDate}
+            onChange={(event) =>
+              setSelectedDate(event.target.value)
+            }
+          >
+            {selectedCourse?.dates?.map((date) => (
+              <option key={date} value={date}>
+                {date}
+              </option>
+            ))}
+          </select>
+        </div>
+      </div>
       <div>
         <h2>Company</h2>
-        <label>NAME*</label>
-        <input
-          type="text"
-          value={companyName}
-          onBlur={() => {
-            isEmpty(companyName) && setInvalidName(true);
-          }}
-          onChange={(event) => {
-            setCompanyName(event.target.value);
-            invalidName &&
-              !isEmpty(event.target.value) &&
-              setInvalidName(false);
-          }}
-        />
+        <div className={labelAndInput}>
+          <label>NAME*</label>
+          <input
+            type="text"
+            value={companyName}
+            onBlur={() => {
+              isEmpty(companyName) && setInvalidName(true);
+            }}
+            onChange={(event) => {
+              setCompanyName(event.target.value);
+              invalidName &&
+                !isEmpty(event.target.value) &&
+                setInvalidName(false);
+            }}
+          />
+        </div>
         {invalidName && <p>Name is required</p>}
-        <label>PHONE*</label>
-        <input
-          type="text"
-          value={companyPhone}
-          onBlur={() => {
-            isEmpty(companyPhone) && setInvalidPhone(true);
-          }}
-          onChange={(event) => {
-            setCompanyPhone(event.target.value);
-            invalidPhone &&
-              !isEmpty(event.target.value) &&
-              setInvalidPhone(false);
-          }}
-        />
+        <div className={labelAndInput}>
+          <label>PHONE*</label>
+          <input
+            type="text"
+            value={companyPhone}
+            onBlur={() => {
+              isEmpty(companyPhone) &&
+                setInvalidPhone(true);
+            }}
+            onChange={(event) => {
+              setCompanyPhone(event.target.value);
+              invalidPhone &&
+                !isEmpty(event.target.value) &&
+                setInvalidPhone(false);
+            }}
+          />
+        </div>
         {invalidPhone && <p>Phone is required</p>}
-        <label>E-MAIL*</label>
-        <input
-          type="text"
-          value={companyEmail}
-          onBlur={() => {
-            !isEmailValid(companyEmail) &&
-              setInvalidEmail(true);
-          }}
-          onChange={(event) => {
-            setCompanyEmail(event.target.value);
-            invalidEmail &&
-              isEmailValid(event.target.value) &&
-              setInvalidEmail(false);
-          }}
-        />
+        <div className={labelAndInput}>
+          <label>E-MAIL*</label>
+          <input
+            type="text"
+            value={companyEmail}
+            onBlur={() => {
+              !isEmailValid(companyEmail) &&
+                setInvalidEmail(true);
+            }}
+            onChange={(event) => {
+              setCompanyEmail(event.target.value);
+              invalidEmail &&
+                isEmailValid(event.target.value) &&
+                setInvalidEmail(false);
+            }}
+          />
+        </div>
       </div>
       {invalidEmail && <p>Email is not valid</p>}
       <div>
@@ -178,59 +195,54 @@ function App() {
                 X
               </button>
             )}
-            <label>NAME*</label>
-            <input
-              type="text"
-              value={participant.name}
-              name="name"
-              onBlur={() => {
-                const current = getParticipant(
-                  participant.id
-                );
-                !current.changed &&
-                  handleChangeParticipant(participant.id, {
-                    // mimics event interface
-                    target: {
-                      name: 'changed',
-                      value: true,
-                    },
-                  });
-              }}
-              onChange={(event) => {
-                handleChangeParticipant(
-                  participant.id,
-                  event
-                );
-              }}
-            />
+            <div className={labelAndInput}>
+              <label>NAME*</label>
+              <input
+                type="text"
+                value={participant.name}
+                name="name"
+                onBlur={() => {
+                  const current = getParticipant(
+                    participant.id
+                  );
+                  !current.changed &&
+                    handleChangeParticipant(
+                      participant.id,
+                      {
+                        // mimics event interface
+                        target: {
+                          name: 'changed',
+                          value: true,
+                        },
+                      }
+                    );
+                }}
+                onChange={(event) => {
+                  handleChangeParticipant(
+                    participant.id,
+                    event
+                  );
+                }}
+              />
+            </div>
             {participants[index].changed &&
               isEmpty(participants[index].name) && (
                 <p>Participant's name is required</p>
               )}
-            <label>PHONE</label>
-            <input
-              type="text"
-              value={participant.phone}
-              name="phone"
-              onChange={(event) => {
-                handleChangeParticipant(
-                  participant.id,
-                  event
-                );
-              }}
-            />
-            <label>E-MAIL</label>
-            <input
-              type="text"
-              value={participant.email}
-              name="email"
-              onChange={(event) => {
-                handleChangeParticipant(
-                  participant.id,
-                  event
-                );
-              }}
-            />
+            <div className={labelAndInput}>
+              <label>E-MAIL</label>
+              <input
+                type="text"
+                value={participant.email}
+                name="email"
+                onChange={(event) => {
+                  handleChangeParticipant(
+                    participant.id,
+                    event
+                  );
+                }}
+              />
+            </div>
           </div>
         ))}
         <button
