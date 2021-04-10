1cef5efa2bec097a152aa68da45517203d4fdc2e
edvinpontuslarsson <el223na@student.lnu.se>
2021-04-10 23:44:58 +0200

    breaks out participants section

--- web/src/components/ApplicationForm.js
+++ web/src/components/ApplicationForm.js
@@ -8,14 +8,12 @@ import {
   labelAndInput,
   phoneAndEmail,
   companySection,
-  participantsSection,
-  participantHeading,
   errorMessage,
   errorBorder,
-  addParticipantButton,
 } from '../utils/classNames';
 import ApplicationSubmitButton from './ApplicationSubmitButton';
 import SubmittedApplications from './SubmittedApplications';
+import ParticipantsSection from './ParticipantsSection';
 
 const ApplicationForm = () => {
   const [courses, setCourses] = useState([]);
@@ -232,107 +230,14 @@ const ApplicationForm = () => {
           </div>
         </div>
       </section>
-
-      <section className={participantsSection}>
-        <h2>Participants</h2>
-        {participants.map((participant, index) => (
-          <div key={participant.id}>
-            <div className={participantHeading}>
-              <h3>Participant #{index + 1}</h3>
-              {index !== 0 && (
-                <button
-                  onClick={() => {
-                    handleRemoveParticipant(participant.id);
-                  }}
-                >
-                  <h3>X</h3>
-                </button>
-              )}
-            </div>
-            <div className={labelAndInput}>
-              <label>NAME*</label>
-              <input
-                type="text"
-                value={participant.name}
-                name="name"
-                onBlur={() => {
-                  const current = getParticipant(
-                    participant.id
-                  );
-                  !current.changed &&
-                    handleChangeParticipant(
-                      participant.id,
-                      {
-                        // mimics event interface
-                        target: {
-                          name: 'changed',
-                          value: true,
-                        },
-                      }
-                    );
-                }}
-                onChange={(event) => {
-                  handleChangeParticipant(
-                    participant.id,
-                    event
-                  );
-                }}
-                className={classnames({
-                  [`${errorBorder}`]:
-                    participants[index].changed &&
-                    isEmpty(participants[index].name),
-                })}
-              />
-              {participants[index].changed &&
-                isEmpty(participants[index].name) && (
-                  <p className={errorMessage}>
-                    Participant's name is required
-                  </p>
-                )}
-            </div>
-
-            <div className={phoneAndEmail}>
-              <div className={labelAndInput}>
-                <label>PHONE</label>
-                <input
-                  type="text"
-                  value={participant.phone}
-                  name="phone"
-                  onChange={(event) => {
-                    handleChangeParticipant(
-                      participant.id,
-                      event
-                    );
-                  }}
-                />
-              </div>
-              <div className={labelAndInput}>
-                <label>E-MAIL</label>
-                <input
-                  type="text"
-                  value={participant.email}
-                  name="email"
-                  onChange={(event) => {
-                    handleChangeParticipant(
-                      participant.id,
-                      event
-                    );
-                  }}
-                />
-              </div>
-            </div>
-          </div>
-        ))}
-        <button
-          className={addParticipantButton}
-          disabled={participants.some(
-            (item) => item.name === ''
-          )}
-          onClick={() => handleAddParticipant()}
-        >
-          Add a participant
-        </button>
-      </section>
+      <ParticipantsSection
+        participants={participants}
+        getParticipant={getParticipant}
+        handleAddParticipant={handleAddParticipant}
+        handleChangeParticipant={handleChangeParticipant}
+        handleRemoveParticipant={handleRemoveParticipant}
+        isEmpty={isEmpty}
+      />
       <ApplicationSubmitButton
         courseApplication={{
           courseId: selectedCourse.id,
