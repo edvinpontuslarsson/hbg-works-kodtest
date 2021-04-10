eedea5e3e71c3ab894874b44201bfa766020c6e4
edvinpontuslarsson <el223na@student.lnu.se>
2021-04-10 21:33:58 +0200

    Application submit button

---
+++ web/src/components/ApplicationSubmitButton.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+import { useToasts } from 'react-toast-notifications';
+
+import { submitButton } from '../utils/classNames';
+
+const ApplicationSubmitButton = ({
+  courseApplication,
+  disabled,
+}) => {
+  const { addToast } = useToasts();
+
+  const handleSubmit = () => {
+    axios
+      .post('/api/applications', {
+        courseApplication,
+      })
+      .then(() => {
+        addToast('Application submitted', {
+          appearance: 'success',
+        });
+      });
+  };
+
+  return (
+    <button
+      className={submitButton}
+      onClick={handleSubmit}
+      disabled={disabled}
+    >
+      Submit application
+    </button>
+  );
+};
+
+export default ApplicationSubmitButton;
