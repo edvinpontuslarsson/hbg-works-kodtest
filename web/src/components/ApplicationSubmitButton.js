5b9ad2fb938e276c472d2a13ce4db3ca0758b197
Edvin Larsson <el223na@student.lnu.se>
2021-04-10 23:02:17 +0200

    Post confirmation clear (#20)
    * toast dependency
    
    * wrap app toastprovider
    
    * toast, uses modificated version, no console err
    
    * Application submit button
    
    * imports submit btn
    
    * classnames in util file
    
    * toast props
    
    * rm unused vars
    
    * clears form
    
    * simplifies clear form
    
    * send status 500
    
    * submission err handling

---
+++ web/src/components/ApplicationSubmitButton.js
@@ -0,0 +1,42 @@
+import axios from 'axios';
+import { useToasts } from 'react-toast-notifications';
+
+import { submitButton } from '../utils/classNames';
+
+const ApplicationSubmitButton = ({
+  courseApplication,
+  disabled,
+  clearForm,
+}) => {
+  const { addToast } = useToasts();
+
+  const handleSubmit = () => {
+    axios
+      .post('/api/applications', {
+        courseApplication,
+      })
+      .then(() => {
+        clearForm();
+        addToast('Application submitted', {
+          appearance: 'success',
+        });
+      })
+      .catch(() => {
+        addToast('Submission failed', {
+          appearance: 'error',
+        });
+      });
+  };
+
+  return (
+    <button
+      className={submitButton}
+      onClick={handleSubmit}
+      disabled={disabled}
+    >
+      Submit application
+    </button>
+  );
+};
+
+export default ApplicationSubmitButton;
---
+++ web/src/utils/classNames.js
@@ -0,0 +1,12 @@
+export const courseAndDate = 'course-and-date';
+export const labelAndInput = 'label-and-input';
+export const phoneAndEmail = 'phone-and-email';
+export const companySection = 'company-section';
+export const participantsSection = 'participants-section';
+export const participantHeading = 'participant-heading';
+export const errorMessage = 'error-message';
+export const errorBorder = 'error-border';
+export const submitButton = 'submit-button';
+export const addParticipantButton =
+  'add-participant-button';
+export const fetchApplications = 'fetch-applications';
