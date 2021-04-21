cc33b7aded8f2698e3e0f1e4bd07024c5bfc0a9e
edvinpontuslarsson <el223na@student.lnu.se>
2021-04-21 20:26:06 +0200

    removes copies

--- server/config/dbHandler.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const dbConnection =
-  process.env.DB_CONNECTION ||
-  'mongodb://mongo:27017/hbg-works-kodtest';
-
-mongoose.connect(dbConnection);
-
-module.exports = mongoose.connection;
