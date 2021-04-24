89e2ce6720566b14a9de27b7f1f3e98fdb1a908c
edvinpontuslarsson <el223na@student.lnu.se>
2021-04-24 20:31:17 +0200

    client can request server

--- web/src/components/ApplicationForm.js
+++ web/src/components/ApplicationForm.js
@@ -55,6 +55,9 @@ const ApplicationForm = () => {
     //   setSelectedCourse(coursesData[0]);
     //   setSelectedDate(coursesData[0]?.dates[0]);
     // });
+    axios.get('http://localhost/api/products').then((payload) => {
+      console.log(payload);
+    });
   }, []);
 
   /**
